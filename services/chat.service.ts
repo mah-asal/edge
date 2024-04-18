8500528d96acfc7a5f74b95c22960f1aa89b8e7e
Mirza <sayheymirza@gmail.com>
2024-04-18 06:54:02 +0000

    chat messages action

--- services/chat.service.ts
+++ services/chat.service.ts
@@ -1,6 +1,7 @@
 import type { ServiceSchema } from "moleculer";
 
 import moment from 'jalali-moment';
+import cheerio from "cheerio";
 
 import api from "../shared/api";
 import endpoint from "../shared/endpoint";
@@ -49,7 +50,7 @@ const ChatService: ServiceSchema = {
 					convert: true,
 					min: 1,
 					max: 100,
-					default: 10
+					default: 20
 				},
 			},
 			async handler(ctx) {
@@ -57,7 +58,7 @@ const ChatService: ServiceSchema = {
 					const { type, page, limit } = ctx.params;
 					const { token } = ctx.meta;
 
-					let path = type == 'admin' ? '/AdminMessage/Index' : '/Chat';					
+					let path = type == 'admin' ? '/AdminMessage/Index' : '/Chat';
 
 					const result = await api.request({
 						path: path += `?pageIndex=${page - 1}&pageSize=${limit}`,
@@ -72,8 +73,8 @@ const ChatService: ServiceSchema = {
 						}
 					});
 
-					const myId = myProfile.code == 200 ? myProfile.data.id : undefined;		
-					
+					const myId = myProfile.code == 200 ? myProfile.data.id : undefined;
+
 					const count = result.totalCount ?? result.returnData.totalCount;
 
 					return {
@@ -94,17 +95,17 @@ const ChatService: ServiceSchema = {
 								seen: type == 'admin' ? this.settings.seen['1'] : this.settings.seen[element.sender.isOnlineByDateTime] ?? 'offline',
 							};
 
-							if(sender.id == myId) {
+							if (sender.id == myId) {
 								sender = {
 									id: item.reciverId,
 									name: item.reciverName,
 									avatar: endpoint.format(item.reciverImage),
-									seen: type == 'admin' ? this.settings.seen['1'] : this.settings.seen[element.reciver.isOnlineByDateTime] ?? 'offline',
+									seen: type == 'admin' ? this.settings.seen['1'] : this.settings.seen[element.reciver?.isOnlineByDateTime ?? '0'] ?? 'offline',
 								};
 							}
 
 							return {
-								id: item.id ?? sender.id,
+								id: type == 'admin' ? item.id : sender.id == myId ? item.reciverId : sender.id,
 								message: this.formatMessage(item),
 								sender,
 							};
@@ -119,6 +120,98 @@ const ChatService: ServiceSchema = {
 				}
 			}
 		},
+		messages: {
+			visibility: 'published',
+			description: 'Get list of messages by pagination',
+			params: {
+				id: {
+					type: "number",
+					convert: true,
+					min: 1
+				},
+				type: {
+					type: "enum",
+					values: ["user", "admin"],
+				},
+				page: {
+					type: 'number',
+					convert: true,
+					min: 1,
+					default: 1
+				},
+				limit: {
+					type: 'number',
+					convert: true,
+					min: 1,
+					max: 100,
+					default: 50,
+				},
+			},
+			async handler(ctx) {
+				try {
+					const { id, type, page, limit } = ctx.params;
+					const { token } = ctx.meta;
+
+					let path = type == 'admin' ? '/AdminMessage/Details' : '/Chat/Details';
+
+					const start = Date.now();
+
+					const result = await api.request({
+						path: path += `?pageIndex=${page - 1}&pageSize=${limit}&id=${id}`,
+						method: 'GET',
+						token,
+					});
+
+					const myProfile: any = await ctx.call('api.v1.profile.me', {}, {
+						meta: {
+							token: token,
+							cache: true,
+						}
+					});
+
+					const myId = myProfile.code == 200 ? myProfile.data.id : undefined;
+
+					let data: any[] = [];
+					let meta: any = {};
+
+					for (let item of (type == 'admin' ? result.returnData.apiDataItem : result.returnData.items)) {
+						data.push(
+							{
+								...this.formatMessage(item),
+								'me': item.senderId == myId,
+							}
+						)
+					}
+
+
+					if (type == 'user') {
+						let total = result.returnData.totalCount;
+
+						meta = {
+							total,
+							last: Math.max(Math.ceil(total / limit), 1),
+						};
+					}
+
+					return {
+						code: 200,
+						meta: {
+							page,
+							limit,
+							...meta,
+							took: Date.now() - start,
+						},
+						data
+					}
+				} catch (error) {
+					console.error(error);
+
+					return {
+						code: 500
+					}
+				}
+			}
+		}
 	},
 
 	/**
@@ -135,30 +228,61 @@ const ChatService: ServiceSchema = {
 				id: item.id,
 				readed: item.isRead,
 				type: 'unknown',
+				message: '',
 				data: '',
-				ago: moment(item.sendDateTime).locale('fa').fromNow(true),
+				ago: item.sendDateTime.includes('ه') ? item.sendDateTime : moment(item.sendDateTime).locale('fa').fromNow(true),
 			};
-			
+
+			item.message = cheerio.load(item.message).text();
 
 			switch (item.contentType) {
 				case 1:
 					output.type = 'text';
+					output.message = item.message.slice(0, 50);
 					output.data = item.message;
 					break;
 
 				case 7:
 					const data = JSON.parse(item.message);
 					output.type = data.type;
-					
+
 					switch (output.type) {
 						case 'text':
+							output.message = data.message.slice(0, 50);
 							output.data = data.message;
 							break;
 
-						case 'voice':
-							output.data = 'پیام صوتی';
+						case 'image':
+							output.message = 'تصویر';
+							output.data = data.media[0];
 							break;
-					
+
+						case 'voice':
+							output.message = 'پیام صوتی';
+							output.data = data.media[0];
+							break;
+
+						case 'call':
+							output.data = data.media[0];
+							let mode = output.data.mode == 'audio' ? 'صوتی' : 'تصویری';
+							if (output.data.type == 'endcall') {
+								output.message = `تماس ${mode} ${output.data.time}`;
+							}
+							if (output.data.type == 'misscall') {
+								output.message = `تماس ${mode} از دست رفته`;
+							}
+							if (output.data.type == 'declined') {
+								output.message = `تماس ${mode} رد شده`;
+							}
+							break;
+						
+						case 'map':
+							output.message = 'موقعیت مکانی';
+							output.data = {
+								...data.media[0]
+							};
+							break;
+
 						default:
 							break;
 					}
